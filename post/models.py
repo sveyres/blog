1e91ec15a108cac7d13e4487adc6332f8c22cfe5
bl00m <leon.pierre@openmailbox.org>
2017-05-11 10:45:22 +0200

    Add django-autoslug package

--- post/models.py
+++ post/models.py
@@ -2,16 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from autoslug import AutoSlugField
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=60, verbose_name='Titre')
+    slug = AutoSlugField(populate_from='title')
     body = models.TextField(verbose_name='Contenu')
     creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Date de création')
 
     def __unicode__(self):
-        return '{0} | {1}'.format(self.creation_date.stfrdate('%d/%m/%Y'), self.title)
+        return '{0} | {1}'.format(self.creation_date, self.title)
 
     class Meta:
         verbose_name = 'Article'
